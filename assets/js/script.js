d30212dc21a6457681f5e35f638a6b416a650ac9
WalleyemanZ <jacobzea96@gmail.com>
2023-01-18 21:51:16 -0700

    added example of questions array

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// var questions = [ // EXAMPLE OF QUESTION ARRAY FOR HW    
+//       question: "What is the capital of France?",
+//       answers: {
+//         1: "Paris",
+//         2: "London",
+//         3: "Berlin",
+//         4: "Rome"
+//       },
+//       correctAnswer: 1
+//     },
+//     {
+//       question: "What is the highest mountain in the world?",
+//       answers: {
+//         1: "Mount Everest",
+//         2: "K2",
+//         3: "Kangchenjunga",
+//         4: "Lhotse"
+//       },
+//       correctAnswer: 1
+//     }
+//   ];
